3eadd97b22c36bb56b8ebb77031acbf217d247fb
tomodevel <!Dhxhzh123>
2020-06-07 23:48:04 +0900

    final code completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import Home from './routes/Home';
 import './App.css';
+import {HashRouter, Route} from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
 function App() {
-  return <Home />
+    return (
+        <HashRouter>
+            <Navigation />
+            <Route path="/home" exact={true} component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/movie-detail" component={Detail}/>
+        </HashRouter>
+    );
 }
 
 export default App;
